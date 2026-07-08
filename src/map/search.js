13018a92b254e4ce3bbc012002af4f3312ccb0b2
xxxily <974278171@qq.com>
2026-07-09 01:47:20 +0800

    style: 调整主路线和备选线不透明度分别为 0.95 和 0.75

--- src/map/search.js
+++ src/map/search.js
@@ -150,14 +150,14 @@ function selectRoute (index) {
       polyline.setStyle({
         color: '#0f766e',
         weight: 7,
-        opacity: 1.0,
+        opacity: 0.95,
       })
       polyline.bringToFront()
     } else {
       polyline.setStyle({
         color: '#94a3b8',
         weight: 5,
-        opacity: 0.6,
+        opacity: 0.75,
       })
     }
   })
@@ -293,7 +293,7 @@ function triggerRoutePlanning (map, AMap) {
         const polyline = L.polyline(pathPoints, {
           color: '#94a3b8',
           weight: 5,
-          opacity: 0.6,
+          opacity: 0.75,
         }).addTo(routeFeatureGroup)
 
         polyline.on('click', (e) => {
